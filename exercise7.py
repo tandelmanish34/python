99da3c69d43ec5fe0b8eb63d8cc1297f0baeee53
tandelmanish34 <113622759+tandelmanish34@users.noreply.github.com>
2022-09-15 20:29:39 -0400

    sample
    sample file

---
+++ exercise7.py
@@ -0,0 +1,20 @@
+firstname = input("Enter First name: ")
+lastname = input("Enter Last name: ")
+dob= int(input("Enter Year of birth: "))
+age = 2022 - dob
+#calculate the age
+
+carname = input ("What car do you own?")
+carage = input ("How many years have you had the car?") 
+#calculate what year
+kids = int(input ("Your kid's age?"))
+
+#calculate the year they were born
+
+
+print ("**Your desciption:**")
+print ("Your name is: ", firstname , lastname)
+print ("Your age is: ", age)
+print ("You own "+ carname + " since " + carage + " years")
+print ("Your kid was born in", 2022-kids)
+print ("**THE END**")
